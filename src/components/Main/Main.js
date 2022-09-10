dbc858df7f264404128ae86532ec2f21b252c69f
vukasin-bulatovic <vukasin.bulatovic.kv@gmail.com>
2022-09-10 15:01:15 +0200

    small bug fix

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,20 +1,22 @@
-import React from 'react';
-import './Main.css';
-import Episodes from '../Episodes/Episodes';
-import Pagination from '../Pagination/Pagination'
+import React from "react";
+import "./Main.css";
+import Episodes from "../Episodes/Episodes";
+import Pagination from "../Pagination/Pagination";
 
 function Main() {
- const [episodes, setEpisodes] = React.useState([])
-  React.useEffect(()=>{
-    fetch('https://rickandmortyapi.com/api/character/?page=1')
-    .then(res => res.json())
-    .then(data => setEpisodes(data.results))
-  }, [])
+  const [episodes, setEpisodes] = React.useState([]);
+  const [pageNumber, setPageNumber] = React.useState(1);
+  console.log(pageNumber);
+  React.useEffect(() => {
+    fetch(`https://rickandmortyapi.com/api/character/?page=${pageNumber}`)
+      .then((res) => res.json())
+      .then((data) => setEpisodes(data.results));
+  }, [pageNumber]);
 
   return (
-    <div className="main">  
-     <Pagination />
-     <Episodes  episodes={episodes}/>
+    <div className="main">
+      <Pagination setPageNumber={setPageNumber} />
+      <Episodes episodes={episodes} />
     </div>
   );
 }
--- src/components/Pagination/Pagination.js
+++ src/components/Pagination/Pagination.js
@@ -1,36 +1,38 @@
-import React from 'react';
-import { MemoryRouter, Route } from 'react-router';
-import { Link } from 'react-router-dom';
-import Pagination from '@material-ui/lab/Pagination';
-import PaginationItem from '@material-ui/lab/PaginationItem';
-import './Pagination.css'
+import React from "react";
+import { MemoryRouter, Route } from "react-router";
+import { Link } from "react-router-dom";
+import Pagination from "@material-ui/lab/Pagination";
+import PaginationItem from "@material-ui/lab/PaginationItem";
+import "./Pagination.css";
 
- function PaginationLink() {
+function PaginationLink({ setPageNumber }) {
   return (
-    <div className='pagination'>
-        <MemoryRouter initialEntries={['/inbox']} initialIndex={0}>
+    <div className="pagination">
+      <MemoryRouter initialEntries={["/inbox"]} initialIndex={0}>
         <Route>
-            {({ location }) => {
+          {({ location }) => {
             const query = new URLSearchParams(location.search);
-            const page = parseInt(query.get('page') || '1', 10);
+            const page = parseInt(query.get("page") || "1", 10);
+            setPageNumber(page);
+
             return (
-                <Pagination
+              <Pagination
                 page={page}
                 count={10}
                 renderItem={(item) => (
-                    <PaginationItem
+                  <PaginationItem
                     component={Link}
-                    to={`/inbox${item.page === 1 ? '' : `?page=${item.page}`}`}
+                    to={`/inbox${item.page === 1 ? "" : `?page=${item.page}`}`}
                     {...item}
-                    />
+                  />
                 )}
-                />
+              />
             );
-            }}
+          }}
         </Route>
-        </MemoryRouter>
+      </MemoryRouter>
     </div>
   );
 }
 
-export default PaginationLink
+export default PaginationLink;
